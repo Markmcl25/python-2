5b0a08a52e19fd38942458deb5182787ddd24551
Mark Mcloughlin <mark25mky@gmail.com>
2024-05-29 15:34:28 +0000

    added function to relay text to google sheet

--- run.py
+++ run.py
@@ -11,3 +11,14 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('python-2')
+
+in_going = SHEET.worksheet('input')
+
+def relay_text_to_sheet(text):
+    """
+    Appends a row with the given text to the Google Sheet.
+
+    Parameters:
+    text (str): The text to relay back to the sheet.
+    """
+    worksheet.append_row([text])
